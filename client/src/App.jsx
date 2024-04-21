4c854f0e3efacc71e6ad5380e3af102bc5e202ef
Charan Singh <charansingh25082002@gmial.com>
2024-04-21 18:41:38 +0530

    created all required folders (empty)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 const App = () => {
   return (
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx';
+import {CssBaseline} from '@mui/material';
+import {HelmetProvider} from 'react-helmet-async';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <CssBaseline/>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 )
